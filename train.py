ae98623750eb94f4f10e21701d0b383f8e1fff45
sathish <gsathish8421@gmail.com>
2023-05-21 03:35:45 +0530

    adding train.py script and updated to utillities.

---
+++ train.py
@@ -0,0 +1,173 @@
+import wandb
+wandb.login(key='24434976526d9265fdbe2b2150787f46522f5da4')
+
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+import torch
+from types import SimpleNamespace
+from torch.nn.utils.rnn import pad_sequence
+import torch.optim as optim
+import random
+from utilities import *
+import argparse
+
+train,valid,test=load_data(data_path,lang)
+
+add_start_end(train)
+add_start_end(valid)
+add_start_end(test)
+
+train_src_chars,train_target_chars=get_unique_chars(train)
+valid_src_chars,valid_target_chars=get_unique_chars(valid)
+test_src_chars,test_target_chars=get_unique_chars(test)
+train_target_chars.add('*') # extra char to handle unknowns in valid and test data.
+    
+src_char_idx,src_idx_char=get_char_map(train_src_chars)
+target_char_idx,target_idx_char=get_char_map(train_target_chars)
+
+encoder_vocab_size=len(src_char_idx)+1 # one extra for padding
+decoder_vocab_size=len(target_char_idx)+1 # one extra for padding
+
+max_seq_length=train[0].apply(lambda x:len(x)).max() # maximum sequence length in latin word
+max_target_length=train[1].apply(lambda x:len(x)).max() # maximum word length of target
+
+
+# vectorize train,valid,test based on characters mapping
+train_src_int,train_target_int=vectorize(train,src_char_idx,target_char_idx,max_seq_length)
+valid_src_int,valid_target_int=vectorize(valid,src_char_idx,target_char_idx,max_seq_length)
+test_src_int,test_target_int=vectorize(test,src_char_idx,target_char_idx,max_seq_length)
+
+
+upto=1000
+train_src_int=train_src_int[:upto]
+train_target_int=train_target_int[:upto]
+
+valid_src_int=valid_src_int[:upto]
+valid_target_int=valid_target_int[:upto]
+
+
+def predict_test(model):
+    ''' Used at the end to calculate test accuracy'''
+    with torch.no_grad():
+        target_end_index=target_char_idx['$']
+        batch_no=0
+        test_acc=0
+        for data in get_batch(test_src_int,test_target_int,config.batch_size):
+            batch_no+=1
+            x=data[0]
+            y=data[1]
+            x=x.to(torch.int64).T
+            y=y.to(torch.int64).T
+            target=y.detach().cpu().numpy()
+            src=x.detach().cpu().numpy()
+            outputs,_=model.forward(x,y,prediction=True) # prediction=True,disables teacher forcing.
+            batch_acc=cal_acc(outputs,y)
+            test_acc+=batch_acc
+        test_acc/=batch_no
+        return test_acc
+    
+            
+    
+
+def main():
+    config=wandb.config
+    config.encoder_vocab_size=encoder_vocab_size
+    config.decoder_vocab_size=decoder_vocab_size
+    config.max_seq_length=max_seq_length
+    
+    if config.attention=='Yes':
+        model=AttentionSeq2Seq(config).to(device) # if attention is enabled
+    else:
+        model=Seq2Seq(config).to(device) # if attention is not enabled.
+        
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.CrossEntropyLoss(ignore_index=0)
+    
+    for epoch in range(config.epochs):
+        train_loss=0
+        train_acc=0
+        model.train()
+        batch_no=0
+        for data in get_batch(train_src_int,train_target_int,config.batch_size):
+#             print(batch_no)
+            batch_no+=1
+            x=data[0]
+            y=data[1]
+            x=x.to(torch.int64).T
+            y=y.to(torch.int64).T
+            outputs,attention_scores=model.forward(x,y)
+            output=outputs.reshape(-1,outputs.shape[2])
+            target=y.reshape(-1)
+            optimizer.zero_grad() # zeroing grads before backpropagtaing gradients
+            target=target-1
+            target[target<0]=0
+            loss = criterion(output, target)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1) # gradient clipping
+            optimizer.step()
+            train_loss+=loss.item()*config.batch_size
+
+            batch_acc=cal_acc(outputs,y)
+            train_acc+=batch_acc
+        train_loss/=len(train_src_int)
+        train_acc/=batch_no # avg train accuracy across all batches
+        model.eval()
+
+        valid_loss=0
+        valid_acc=0
+        batch_no=0
+        with torch.no_grad():
+            for data in get_batch(valid_src_int,valid_target_int,config.batch_size):
+                batch_no+=1
+                x=data[0]
+                y=data[1]
+                x=x.to(torch.int64).T
+                y=y.to(torch.int64).T
+                outputs,attention_scores=model.forward(x,y,prediction=True)
+                output=outputs.reshape(-1,outputs.shape[2])
+                target=y.reshape(-1)
+                target=target-1
+                target[target<0]=0
+                loss = criterion(output, target)
+                valid_loss+=loss.item()*config.batch_size
+                valid_acc+=cal_acc(outputs,y)
+            valid_loss/=len(valid_src_int)
+            valid_acc/=batch_no
+        print(f'Epoch: {epoch+1} Train Loss: {train_loss:.4f} Valid Loss: {valid_loss:.4f} Train Acc: {train_acc:.4f}  Valid Acc: {valid_acc:.4f}')
+        wandb.log({'train accuracy':train_acc,'train loss':train_loss,'valid accuracy':valid_acc,'valid loss':valid_loss})
+    return model
+
+if __name__=="__main__": 
+    
+    #adding support for command line arguments
+    parser = argparse.ArgumentParser(description = 'Set Hyper Parameters')
+    parser.add_argument('-wp'   , '--wandb_project'  , type = str  , default='CS22M080',metavar = '', help = 'WandB Project Name (Non-Empty String)')
+    parser.add_argument('-we'   , '--wandb_entity'   , type = str  , default='CS22M080',metavar = '', help = 'WandB Entity Name (Non-Empty String)')
+    parser.add_argument('-e'    , '--epochs'         , type = int  , default=15,metavar = '', help = 'Number of Epochs (Positive Integer)')
+    parser.add_argument('-b'    , '--batch_size'     , type = int  , default=256,metavar = '', help = 'Batch Size (Positive Integer)')
+    parser.add_argument('-el'  , '--encoder_num_layers'     , type = int  , default=3,metavar = '', help = '')
+    parser.add_argument('-dl'  , '--decoder_num_layers'     , type = int  , default=2,metavar = '', help = '')
+    parser.add_argument('-hs'   , '--hidden_size'  , type = int  , default=512,metavar = '', help = 'Cell hidden size')
+    parser.add_argument('-es'   , '--embedding_size'  , type = int  , default=256,metavar = '', help = 'embedding size')
+    parser.add_argument('-do'   , '--dropout'  , type = float  , default=0.2,metavar = '', help = 'Dropout in cell')
+    parser.add_argument('-bi'   , '--bidirectional'  , type = str  , default="Yes",choices=['Yes','No'])
+    parser.add_argument('-ct'   , '--cell_type'  , type = str  , default="GRU",choices=['LSTM','GRU','RNN'])
+    parser.add_argument('-at'   , '--attention'  , type = str  , default="Yes",choices=['Yes','No'])
+    
+    
+    params = vars(parser.parse_args())
+    wandb.init(project=params['wandb_project'],config=params)
+    config=wandb.config
+    run_name=f'Attention {config.attention} - Cell-{config.cell_type} Hidden-{config.hidden_size} Embedding-{config.embedding_size} Bidir-{config.bidirectional} Dropout -{config.dropout} EL-{config.encoder_num_layers} DL-{config.decoder_num_layers}'
+    wandb.run.name=run_name
+    model=main()
+    print("training completed")
+    test_accuracy=predict_test(model)
+    print("*"*50)
+    print(f"Final Test accuracy: {test_accuracy:.2f}")
+    print("*"*50)
+    wandb.log({'test accuracy':test_accuracy})
+    wandb.finish()
+    
+    
--- utilities.py
+++ utilities.py
@@ -14,17 +14,20 @@ device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def load_data(path,lang):
+    ''' reading data from path based on the lang '''
     train=pd.read_csv(path+lang+'/'+lang+'_train.csv',header=None)
     valid=pd.read_csv(path+lang+'/'+lang+'_valid.csv',header=None)
     test=pd.read_csv(path+lang+'/'+lang+'_test.csv',header=None)
     return train,valid,test
 
 def add_start_end(df):
+    ''' adding end charcter to the source, start and end character to the target'''
     df[0]=df[0].apply(lambda x: x+'$')
     df[1]=df[1].apply(lambda x: '^'+x+'$')
 
 
 def get_unique_chars(df):
+    ''' return source and target unique chars'''
     src_chars=set()
     target_chars=set()
 
@@ -36,11 +39,13 @@ def get_unique_chars(df):
     return src_chars,target_chars
 
 def get_char_map(chars):
+    ''' map chars to index and return the dict'''
     src_char_idx=dict([(char,idx) for idx,char in enumerate(chars,start=1)])
     src_idx_char=dict([(idx, char) for char, idx in src_char_idx.items()])
     return src_char_idx,src_idx_char
 
 def vectorize(df,src_char_idx,target_char_idx,max_seq_length):
+    ''' Convert words to vectors based on the char mapping'''
     src_int=[]
     for word in df[0]:
         word_int=[]
@@ -59,6 +64,7 @@ def vectorize(df,src_char_idx,target_char_idx,max_seq_length):
     return pad_sequence(src_int)[:,1:].to(device).T,pad_sequence(target_int).to(device).T
 
 def get_batch(src,target,batch_size):
+    ''' return batch of data. will reset after epoch is over'''
     num_batches=len(src)//batch_size
     for i in range(num_batches):
         start=i*batch_size
@@ -66,6 +72,7 @@ def get_batch(src,target,batch_size):
         yield src[start:end],target[start:end] 
 
 def cal_acc(pred,actual):
+    '''  calculate accuracy based on prediction and actual. Pred points to  probabilties'''
     pred=pred.clone()
     pred=torch.argmax(pred,2)+1
     pred=pred[1:]
@@ -78,6 +85,7 @@ def cal_acc(pred,actual):
     return acc
 
 def decode_src(src_int,src_idx_char):
+    ''' map back source vector to string representation  '''
     s=""
     for each in src_int:
         if each==0:
@@ -86,6 +94,7 @@ def decode_src(src_int,src_idx_char):
     return s
 
 def decode_target(target_int,target_idx_char,target_end_index):
+    ''' map back target vector to string representation  '''
     s=""
     for each in target_int[1:]:
         if each==target_end_index:
@@ -97,6 +106,12 @@ def decode_target(target_int,target_idx_char,target_end_index):
         
         
 class Encoder(nn.Module):
+    ''' 
+     class structure for Encoder.
+     Based on config:dict provided it will instantinate the fields.
+     Note that in forward, when called with GRU/RNN  we are returning None for cell so as match same shapes at every return.
+     
+    '''
     def __init__(self,config):
         super().__init__()
         self.hidden_size = config.hidden_size
@@ -133,6 +148,12 @@ class Encoder(nn.Module):
         
 
 class Decoder(nn.Module):
+    ''' 
+     class structure for Decoder.
+     Based on config:dict provided it will instantinate the fields.
+     Note that in forward, when called with GRU/RNN  we are returning None for cell so as match same shapes at every return.
+     
+    '''
     def __init__(self, config):
         super().__init__()
         self.hidden_size = config.hidden_size
@@ -167,6 +188,10 @@ class Decoder(nn.Module):
         return preds, hidden, cell
     
 class Seq2Seq(nn.Module):
+    ''' 
+     Combines Encoder and Decoder classes so as to construct Sequence to sequence model.
+     
+    '''
     def __init__(self,config):
         super().__init__()
         self.encoder = Encoder(config)
@@ -175,12 +200,13 @@ class Seq2Seq(nn.Module):
         self.config=config
 
     def forward(self,source,target,prediction=False):
+         # if Prediction is True, then teacher forcing will be disbaled.
       
         outputs = torch.zeros(target.shape[0], self.batch_size,self.config.decoder_vocab_size).to(device)
         
         cell=None 
         hidden, cell,_ = self.encoder(source)
-        if self.encoder.bidirectional:
+        if self.encoder.bidirectional: # take mean of the last states if it's bidirectional
                 hidden=hidden[[self.encoder.num_layers-1,-1]]
                 hidden=hidden.mean(axis=0).unsqueeze(0)
                 
@@ -195,7 +221,7 @@ class Seq2Seq(nn.Module):
             if self.config.cell_type=='LSTM':
                 cell=cell[-1,:,:]
                 cell=cell.unsqueeze(0)
-        hidden=hidden.repeat(self.config.decoder_num_layers,1,1)
+        hidden=hidden.repeat(self.config.decoder_num_layers,1,1) # repeat it to match decoder num layers
         
         if self.config.cell_type=='LSTM':
             cell=cell.repeat(self.config.decoder_num_layers,1,1)
@@ -217,6 +243,9 @@ class Seq2Seq(nn.Module):
 
 
 class AttentionDecoder(nn.Module):
+    '''
+    Decoder class when working with Attention.
+    '''
     def __init__(self, config):
         super().__init__()
         self.hidden_size = config.hidden_size
@@ -227,7 +256,7 @@ class AttentionDecoder(nn.Module):
         self.fc = nn.Linear(config.hidden_size, config.decoder_vocab_size)
         
         if self.num_layers==1:
-            dropout=0
+            dropout=0 # if layers is 1, make dropout 0.
         else:
             dropout=config.dropout
         
@@ -245,11 +274,7 @@ class AttentionDecoder(nn.Module):
         else:
             self.attn_combined = nn.Linear(self.hidden_size*2 +config.embedding_size, self.hidden_size)
             
-#         self.cell = nn.GRU(config.hidden_size, config.hidden_size,config.decoder_num_layers)
-        
 
-            
-            
 
     def forward(self, x, hidden, cell=None,encoder_outputs=None):
        
@@ -266,16 +291,12 @@ class AttentionDecoder(nn.Module):
         
         
         if(self.cell_type=='LSTM'):
-#             print("dlstm ",cell.shape)
             output, (hidden, cell) = self.cell(output, (hidden, cell))
         else:
             output, hidden = self.cell(output, hidden)
-        
-        # outputs shape: (1, batch_size, hidden_size)
 
         preds = self.fc(output)
         
-        # preds: (1,N,target_vocab_size)
 
         preds = preds.squeeze(0)
         
@@ -284,6 +305,9 @@ class AttentionDecoder(nn.Module):
 
 
 class AttentionSeq2Seq(nn.Module):
+    '''
+    Combines Encoder and AttentionDecoder classes so as to make single model for Seq2seq.
+    '''
     def __init__(self,config):
         super().__init__()
         self.encoder = Encoder(config)
@@ -296,6 +320,8 @@ class AttentionSeq2Seq(nn.Module):
     def forward(self,source,target,prediction=False):
         # source  (Sequnce_lenghth,batch_size)
         # target: (max_traget_length,batch_size)
+
+         # if Prediction is True, then teacher forcing will be disbaled.
         
         outputs = torch.zeros(target.shape[0], self.batch_size,self.config.decoder_vocab_size).to(device)
         attention_scores = torch.zeros(target.shape[0], self.batch_size,self.config.max_seq_length).to(device)
@@ -317,10 +343,8 @@ class AttentionSeq2Seq(nn.Module):
             if self.config.cell_type=='LSTM':
                 cell=cell[-1,:,:]
                 cell=cell.unsqueeze(0)
-            
-#         print("hidden ",hidden.shape) 
-#         print("cell ",cell.shape) 
-        hidden=hidden.repeat(self.config.decoder_num_layers,1,1)
+
+        hidden=hidden.repeat(self.config.decoder_num_layers,1,1) # repeat it to match decoder num layers
         
         if self.config.cell_type=='LSTM':
             cell=cell.repeat(self.config.decoder_num_layers,1,1)
@@ -328,7 +352,6 @@ class AttentionSeq2Seq(nn.Module):
         x = target[0]
         
         for t in range(1, target.shape[0]):
-#             print(t)
             output, hidden, cell,attention_scores[t] = self.decoder(x, hidden, cell,encoder_outputs)
             outputs[t] = output
             max_prob = output.argmax(1)
@@ -337,7 +360,5 @@ class AttentionSeq2Seq(nn.Module):
                 x = target[t] if random.random() < 0.5 else max_prob
             else:
                 x=max_prob
-#         print("outputs",outputs.shape)
-#         print("returning")
         return outputs,attention_scores
  
